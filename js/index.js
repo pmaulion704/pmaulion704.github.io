b4ace35fecaf7622f2b630e1fc703e3e70fe8401
Patrick <patrick.dionisio.maulion@gmail.com>
2018-05-29 07:53:08 -0400

    Added 2 projects
    Added ABR and MiniHackathon projects. More work on Preqin Pro..

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
 $(function() {
 	var baseTitle = window.document.title;
-	var images = {
-		quickSearch: ["http://res.cloudinary.com/pra1se/image/upload/v1523584090/Old_Design_aqlwgt.jpg",
-					  "http://res.cloudinary.com/pra1se/image/upload/v1523988498/ITERATION_1_pfd16q.jpg",	
-					  "http://res.cloudinary.com/pra1se/image/upload/v1523980697/ITERATION_2_m2ocbc.jpg"],
-		profile: ["http://res.cloudinary.com/pra1se/image/upload/v1525691752/PREVIOUS_LAYOUT_gfu45h.jpg"]
-		};
-	var labels = {
-		quickSearch: ["OLD SEARCH", "ITERATION #1", "ITERATION #2"],
-		profile: ["PREVIOUS PROFILE UI"]
-	}
-
-
 
 	// Browser Tab Functionality
 	$(window).focus(function() {
@@ -29,62 +17,28 @@ $(function() {
 
 	// Project Navigation
 	$("#neoProject").click(function(){
-		window.location.href = "PreqinPro.html";
+		window.location.href = "Preqin-Pro.html";
 	});	
 
 	$("#neoProjectTitle").click(function(){
-		window.location.href = "PreqinPro.html";
+		window.location.href = "Preqin-Pro.html";
 	});
 
 	$("#abrProject").click(function(){
-		window.open('ABR.html', '_blank');
+		window.location.href = "Always-Be-Running.html";
 	});	
 
 	$("#abrProjectTitle").click(function(){
-		window.open('ABR.html', '_blank');
+		window.location.href = "Always-Be-Running.html";
 	});
 
+	$("#boostMySchoolProject").click(function(){
+		window.location.href = "Boost-My-School.html";
+	});	
 
-	// Preqin Pro Click Events
-	// Open Modal
-	function openModal(param1, param2){
-		$("#select-img").attr("src", param1);
-		$("#modal-header").html(param2);
-		$(".modal-bg").css("display", "block");
-		$("body").addClass("modal-open");
-		$("#modal-bg").scrollTop(0);
-	};
-	// Quick Search Pics
-	$("#qs-img-1").click(function(){
-		openModal(images.quickSearch[0], labels.quickSearch[0]);
+	$("#boostMySchoolProjectTitle").click(function(){
+		window.location.href = "Boost-MySchool.html";
 	});
-
-	$("#qs-img-2").click(function(){
-		openModal(images.quickSearch[1], labels.quickSearch[1]);
-	});
-
-	$("#qs-img-3").click(function(){
-		openModal(images.quickSearch[2], labels.quickSearch[2]);
-	});
-
-	// Profile Pice
-	$("#pro-img-1").click(function(){
-		openModal(images.profile[0], labels.profile[0]);
-	});
-
-
-	// Close Modal
-	$("#modal").on('click', function(event) {
-		event.stopPropagation();
-	});
-
-	$(".modal-bg").click(function(){
-		$(".modal-bg").css("display", "none");
-		$("body").removeClass("modal-open");
-	});
-		
-
-
 
 	// End of index.js
 });
