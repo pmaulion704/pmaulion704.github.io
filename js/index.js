f5225be296d780d7b18c956024e876fc66eca540
Patrick <patrick.dionisio.maulion@gmail.com>
2019-10-04 15:20:27 -0400

    add hover-over animations across view projects / studies

--- js/index.js
+++ js/index.js
@@ -5,6 +5,16 @@ $(function() {
   		return false;
 	});
 
+	$(".viewHover").mouseenter(function(){
+		$(this).find('.arrow').css('transform', 'translateX(10px)')
+		$(this).find('.arrow').css('transition', '0.25s ease-out')
+	});
+
+	$(".viewHover").mouseleave(function(){
+		$(this).find('.arrow').css('transform', 'translateX(0px)')
+		$(this).find('.arrow').css('transition', '0.25s ease-out')
+	});
+
 	// Add Scroll Functionality
 // End of indjex.js
 });
